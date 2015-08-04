0367d8446bfcb897c25fec42b2001a1633ae789b
Prasad Surase <prasadsurase@gmail.com>
2015-08-04 12:42:32 +0530

    Added code for word count

---
+++ src/word-count.js
@@ -0,0 +1,16 @@
+var wordCount = function (sentence){
+  word_count = {};
+  words = sentence.split(' ');
+  for(i=0; i< words.length; i++){
+    if(words[i] in word_count){
+      word_count[words[i]]++;
+    }
+    else{
+      word_count[words[i]] = 1;
+    }
+  }
+  console.log(word_count);
+  return word_count;
+};
+
+wordCount('olly olly in come free');
